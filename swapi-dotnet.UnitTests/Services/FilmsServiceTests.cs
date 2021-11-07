92c47220f94bba5e7c75db5a3e91312038a2bdb9
Lars Aaberg <lars@aaberg.cc>
2021-11-07 14:30:55 +0100

    Added some unit tests and a simple github actions configuration file

---
+++ swapi-dotnet.UnitTests/Services/FilmsServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Swapi.Dotnet.Services;
+using Xunit;
+
+namespace swapi_dotnet.UnitTests.Services
+{
+    public class FilmsServiceTests
+    {
+        private IFilmsService _filmsService;
+
+        public FilmsServiceTests()
+        {
+            _filmsService = new FilmsService();
+        }
+
+        [Fact]
+        public async Task GetFilms_ReturnsList_Success()
+        {
+            var films = (await _filmsService.GetFilms()).ToArray();
+            
+            films.Should().HaveCountGreaterThan(1);
+            films[0].Id.Should().BeGreaterThan(0);
+            films[0].Fields.EpisodeId.Should().BeGreaterThan(0);
+            films[0].Fields.Planets.Should().NotBeNullOrEmpty();
+        }
+    }
+}
---
+++ swapi-dotnet.UnitTests/Services/PeopleServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Swapi.Dotnet.Services;
+using Xunit;
+
+namespace swapi_dotnet.UnitTests.Services
+{
+    public class PeopleServiceTests
+    {
+        private readonly IPeopleService _peopleService;
+
+        public PeopleServiceTests()
+        {
+            _peopleService = new PeopleService();
+        }
+
+        [Fact]
+        public async Task GetPeople_ReturnsList_Success()
+        {
+            var people = (await _peopleService.GetPeople()).ToArray();
+            
+            people.Should().HaveCountGreaterThan(1);
+            people[0].Id.Should().BeGreaterThan(0);
+            people[0].Fields.Name.Should().NotBeNullOrEmpty();
+        }
+    }
+}
