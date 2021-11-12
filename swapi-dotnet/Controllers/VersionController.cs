29a5eac15769e3ed8c74d340986f196c92af2e57
Aaberg, Lars <lars.aaberg@sbanken.no>
2021-11-12 13:59:17 +0100

    Added versioning

---
+++ swapi-dotnet/Controllers/VersionController.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Swapi.Dotnet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public string GetVersion()
+        {
+            return Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
+        }
+    }
+}
