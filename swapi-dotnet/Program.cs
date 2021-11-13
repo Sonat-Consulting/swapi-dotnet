bcab1c4c2af0ddb8c5f04456c56f3fc2b6d33edc
Lars Aaberg <lars@aaberg.cc>
2021-11-13 16:29:19 +0100

    Application insights sink

--- swapi-dotnet/Program.cs
+++ swapi-dotnet/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Console;
 using Swapi.Dotnet;
 using swapi_dotnet;
 
@@ -13,6 +16,10 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(builder =>
+            {
+                builder.AddApplicationInsights();
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
